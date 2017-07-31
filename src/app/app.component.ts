50f9045e57d57388af2de6cbe503a7b256066ec8
Łukasz Żygłowicz <lzyglowicz@transactorgsl.com>
2017-07-31 18:59:01 +0200

    fgh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app tttttt works!';
+  title = 'To moze byc twoja strona  Krzysiek od Beaty !!!';
 }
